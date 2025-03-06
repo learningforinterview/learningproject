6477e5f1a0bac0cfb70a9501241b5ff4548d0229
tejarshitechnical <tejarshitechnical@gmail.com>
2025-03-06 19:14:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,13 +5,7 @@ terraform {
         version = "=3.0.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name     = "AKSargoCdPractice"
-    storage_account_name    = "techmterraformstorage"
-    subscription_id         = "36d08588-2d3e-46a1-915e-25a7e1cc8e95"
-    container_name          = "tfstate"
-    key                     = "GitHub-Terraform-rg-connectivity-001"
-  }
+  
 }
 provider "azurerm" {
   features {}
